b9bfeb7f94eb220e2b9aba3b05505310480229c4
John Carpenter <jc19980912@gmail.com>
2023-10-25 00:05:26 +1000

    feat:change user entity

--- server/entities/User.entity.ts
+++ server/entities/User.entity.ts
@@ -6,26 +6,17 @@ import { CoreEntity } from "./Core.entity";
   name: "user",
 })
 export class UserEntity extends CoreEntity {
+  @Column({ name: "first_name", nullable: true })
+  first_name?: string;
+
+  @Column({ name: "last_name", nullable: true })
+  last_name?: string;
+
   @Column({ name: "email", nullable: true })
   email?: string;
 
-  @Column({ name: "country_code", nullable: true })
-  countryCode?: string;
-
-  @Column({ name: "phone_number", nullable: true })
-  phoneNumber?: string;
-
   @Column({ name: "password", select: false })
   password: string;
-
-  @Column({ name: "display_name", nullable: true })
-  displayName?: string;
-
-  @Column({ name: "avatar", nullable: true })
-  avatar?: string;
-
-  @Column({ name: "notification_setting", length: 3 })
-  notificationSetting: string;
 
   @Column({ name: "role", default: 0 })
   role: number;
