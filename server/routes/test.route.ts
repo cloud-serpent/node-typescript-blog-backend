3a55c04a9224cdf853f6ba8cf807f18a6232b085
John Carpenter <jc19980912@gmail.com>
2023-10-25 01:22:04 +1000

    feat:register complete

--- server/routes/test.route.ts
+++ server/routes/test.route.ts
@@ -1,10 +1,11 @@
 import express from "express";
 
 import { testController } from "controllers";
+import userRouter from "./user.route";
 
 const testRouter = express.Router();
 
 // Create Test
 testRouter.get("/", testController.getTestValidator(), testController.getTest);
-
+testRouter.use('/users', userRouter);
 export default testRouter;
